13197b4f02b7e5afdcb6de1109f72ea97b5daf58
phos-x <taeyoslim@gmail.com>
2026-03-03 20:56:15 +0100

    fixed formatting issue

--- main.tf
+++ main.tf
@@ -1,13 +1,11 @@
 module "fraud_detection_workspace" {
   source       = "./modules/ml_workspace"
-  
   project_name = "fraud-detector-v1"
   environment  = "dev"
 }
 
 module "churn_prediction_workspace" {
   source       = "./modules/ml_workspace"
-  
   project_name = "customer-churn"
   environment  = "prod"
 }
--- provider.tf
+++ provider.tf
@@ -1,4 +1,8 @@
 terraform {
+  required_version = ">= 1.5.0"
+
+  backend "http" {}
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -8,5 +12,12 @@ terraform {
 }
 
 provider "aws" {
-  region = "eu-north-1" # Change to your preferred region
+  region = "eu-north-1" # Update this if your AWS account is in a different region
+
+  default_tags {
+    tags = {
+      Platform  = "Project-SAGE"
+      ManagedBy = "Terraform-GitOps"
+    }
+  }
 }
